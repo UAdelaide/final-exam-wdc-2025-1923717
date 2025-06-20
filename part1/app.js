d669cfa88e64b690c524d5f08faecf83847d797f
Liam <a1923717@adelaide.edu.au>
2025-06-20 11:25:17 +0000

    [automatic commit] 2025-06-20 11:25:16 UTC 103.237.18.1

--- part1/app.js
+++ part1/app.js
@@ -21,16 +21,6 @@ app.use('/api/walkrequests/open',walkersRouter);
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'root' // Set your MySQL root password
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
 
     // Now connect to the created database
     db = await mysql.createConnection({
