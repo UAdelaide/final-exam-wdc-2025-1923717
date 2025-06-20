81509e2c5b4568ae9d13954373b70fd1564821d4
Liam <a1923717@adelaide.edu.au>
2025-06-20 11:05:35 +0000

    [automatic commit] 2025-06-20 11:05:34 UTC 103.237.18.1

--- part1/dogs.js
+++ part1/dogs.js
@@ -1,13 +1,12 @@
 router.get('/api/dogs', async (req, res) => {
-  try {
-    const [rows] = await db.query(`
+  const [rows] = await db.query(`
     SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
     FROM BookListings bl
     JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
     JOIN Users u ON bl.SellerID = u.UserID
   `);
   res.json(rows);
-)});
+});
 
 app.get('/api/dogs', async (req, res) => {
   try {
