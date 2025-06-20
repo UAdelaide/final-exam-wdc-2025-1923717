14d25afec16579263ff2ce52b21c06ad0c44c448
Liam <a1923717@adelaide.edu.au>
2025-06-20 09:47:41 +0000

    [automatic commit] 2025-06-20 09:47:40 UTC 103.237.18.1

--- part1/app.js
+++ part1/app.js
@@ -4,3 +4,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
