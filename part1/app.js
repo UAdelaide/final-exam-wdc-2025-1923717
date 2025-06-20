8c8f360e8242bfcbadb5f58f6dc29f18d0ccf935
Liam <a1923717@adelaide.edu.au>
2025-06-20 10:10:53 +0000

    [automatic commit] 2025-06-20 10:10:52 UTC 103.237.18.1

--- part1/app.js
+++ part1/app.js
@@ -77,7 +77,7 @@ app.get('/api/books', async (req, res) => {
     const [books] = await db.execute('SELECT * FROM books');
     res.json(books);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 module.exports = app;
