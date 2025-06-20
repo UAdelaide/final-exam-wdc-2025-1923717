c2640e8e1f8d15f47a286c620800b8830527ba1c
Liam <a1923717@adelaide.edu.au>
2025-06-20 11:12:37 +0000

    [automatic commit] 2025-06-20 11:12:36 UTC 103.237.18.1

--- part1/dogs.js
+++ part1/dogs.js
@@ -11,8 +11,8 @@ router.get('/api/dogs', async (req, res) => {
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [dogs] = await db.query('SELECT Dogs.name, Dogs.size
-        FROM Dogs  ');
+    const [dogs] = await db.query('SELECT Dogs.name,
+    Dogs.size FROM Dogs');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Dogs' });
