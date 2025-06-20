72b16cbf66f3f6c40d788b7ca4fbf70bb23fe444
Liam <a1923717@adelaide.edu.au>
2025-06-20 09:50:33 +0000

    [automatic commit] 2025-06-20 09:50:32 UTC 103.237.18.1

--- part1/app.js
+++ part1/app.js
@@ -11,3 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var messagesRouter = require('./routes/messages');
