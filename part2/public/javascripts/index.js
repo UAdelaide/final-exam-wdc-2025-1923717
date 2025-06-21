1344d568f262a0401f5848fa836dae45aea1a535
Liam <a1923717@adelaide.edu.au>
2025-06-21 03:38:12 +0000

    [automatic commit] 2025-06-21 03:38:11 UTC 103.237.18.1

--- part2/public/javascripts/index.js
+++ part2/public/javascripts/index.js
@@ -8,6 +8,29 @@ document.getElementById('app').addEventListener('submit', function(event) {
         alert('Please enter a Username and Password');
         return;
     }
+
+    async function SendLoginCredentials(username, password) {
+    try {
+  const response = await fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password})
+        });
+        const data = await response.json();
+        if (user.role === 'owner') {
+            redirect('/owner-dashboard.html');
+        }
+        else if (user.role === 'walker') {
+            redirect('/walker-dashboard.html');
+        }
+} catch (error) {
+   console.error('Error during login:', error);
+}
+SendLoginCredentials(username, password);
+}
+
     SendLoginCredentials(username, password);
 
 });
