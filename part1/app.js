b40eb572357230e283e716d20952f63de36cc0db
Liam <a1923717@adelaide.edu.au>
2025-06-21 01:17:25 +0000

    [automatic commit] 2025-06-21 01:17:24 UTC 103.237.18.1

--- part1/app.js
+++ part1/app.js
@@ -64,7 +64,7 @@ app.get('/api/dogs', async (req, res) => {
   try {
     const [dogs] = await db.query(`SELECT Dogs.name, Dogs.size
     FROM Dogs
-    Join Users ON owner.username = Dogs.Owner.username`);
+    `);
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Dogs' });
