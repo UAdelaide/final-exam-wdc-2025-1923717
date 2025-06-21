cff4c37d5d289be8c3177f062279440aaa6b01e6
Liam <a1923717@adelaide.edu.au>
2025-06-21 03:20:48 +0000

    [automatic commit] 2025-06-21 03:20:47 UTC 103.237.18.1

--- part2/public/javascripts/index.js
+++ part2/public/javascripts/index.js
@@ -14,6 +14,6 @@ document.getElementById('app').addEventListener('submit', function(event) {
 
 async function SendLoginCredentials(username, password) {
     try{
-        
+        const response = await
     }
 }
